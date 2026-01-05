0364b72c1044b1552bf12a736419d9cc6b92e27f
alrahimi <3719239+alrahimi@users.noreply.github.com>
2026-01-05 10:14:50 -0500

    Update SafeMultiD_Array_BasedOnCUJArticle.h: improve RangeException, const-correctness, size_t indices, noexcept/constexpr

--- SafeMultiD_Array_BasedOnCUJArticle.h
+++ SafeMultiD_Array_BasedOnCUJArticle.h
@@ -1,106 +1,86 @@
+#pragma once
 
-/*
-Based On publsihed in the following July 1997
-https://jacobfilipp.com/DrDobbs/articles/CUJ/1997/9707/rahimi/list1.htm
-https://jacobfilipp.com/DrDobbs/articles/CUJ/1997/9707/rahimi/list2.htm
+#include <cstddef>
+#include <stdexcept>
+#include <string>
 
+// RangeException: stores the dimension, valid low/high range and the offending index.
+// Inherits from std::out_of_range so it integrates with standard exception handling.
+struct RangeException : std::out_of_range {
+    std::size_t dim;
+    std::size_t low;
+    std::size_t high;
+    std::size_t index;
 
-*/
+    RangeException(std::size_t d, std::size_t l, std::size_t h, std::size_t i)
+        : std::out_of_range(build_message(d, l, h, i)), dim(d), low(l), high(h), index(i) {}
 
-#include<iostream>
-
-using namespace std;
-
-//Listing 1: Defines template array class
-
-
-/*
-make it like boost.Just type and size are declared
-typedef boost::multi_array<double, 2> array_type;
-*/
-
-class RangeException
-{
-public:
- RangeException(int dim,int low, int high, int index) 
- {
-  m_dim = dim;
-  m_low = low;
-  m_high = high;
-  m_index = index;
- }
-     
- int m_dim,m_low,m_high,m_index;
-};
-     
-template <class Type,
-size_t size ,int D=1> class A1D 
-{
-public:
-int low=0;
-int high = size -1;
-int m_dim =1;
-     
- Type& operator[](const int i)
- {
-  if(i>=size)
-  {
-    //throw(RangeException(m_dim,0,size-1,i));
-    throw(RangeException(dim(),0,size-1,i));
-  }
-     
-  return element[i];
- }
-     
-     
- int begin() const {return low;}
- int end()   const {return high;}
- int dim()   const {return m_dim;}
-     
 private:
- Type element[size];
-};
-     
-template <class Type,  
- size_t size_1,
- size_t size_2,int D=2> 
- class A2D : public A1D<A1D<Type,size_2>,size_1,D>
-{
-  int m_dim =2;
-  public:
-  int dim()   const {return m_dim;}
-     
-     
-};
-     
-template <class Type, 
- size_t size_1,
- size_t size_2,
- size_t size_3, int D=3> 
- class A3D : public A1D<A2D<Type,size_2,size_3>,size_1>
-{
-  int m_dim =3;
-  public:
-  int dim()   const {return m_dim;}
-     
- 
-     
+    static std::string build_message(std::size_t d, std::size_t l, std::size_t h, std::size_t i) {
+        return "Dimension " + std::to_string(d) + ": subscript " + std::to_string(i) +
+               " is out of range [" + std::to_string(l) + ":" + std::to_string(h) + "]";
+    }
 };
 
-template <class Type, 
- size_t size_1,
- size_t size_2,
- size_t size_3,
- size_t size_4,
- int D=4> 
- class A4D : public A1D<A3D<Type,size_2,size_3,size_4>,size_1>
-{
-  int m_dim =4;
-  public:
-  int dim()   const {return m_dim;}
-     
-};    
-     
-     
-//End of File
+// 1D array template with checked access.
+template <typename Type, std::size_t Size, int D = 1>
+class A1D {
+public:
+    static constexpr int Dim = D;
+    using value_type = Type;
+    using reference = Type&;
+    using const_reference = const Type&;
+    static constexpr std::size_t size = Size;
 
+    A1D() = default;
+
+    reference operator[](std::size_t i) {
+        if (i >= Size) throw RangeException(D, 0, Size - 1, i);
+        return element[i];
+    }
+
+    const_reference operator[](std::size_t i) const {
+        if (i >= Size) throw RangeException(D, 0, Size - 1, i);
+        return element[i];
+    }
+
+    constexpr std::size_t begin() const noexcept { return 0; }
+    constexpr std::size_t end()   const noexcept { return (Size == 0) ? 0 : (Size - 1); }
+    static constexpr int dim() noexcept { return D; }
+
+private:
+    Type element[Size];
+};
+
+// 2D array: outer dimension size_1, inner dimension size_2
+template <typename Type, std::size_t size_1, std::size_t size_2>
+class A2D : public A1D<A1D<Type, size_2, 1>, size_1, 2> {
+public:
+    static constexpr int Dim = 2;
+    static constexpr std::size_t Size1 = size_1;
+    static constexpr std::size_t Size2 = size_2;
+    using base = A1D<A1D<Type, size_2, 1>, size_1, 2>;
+    using typename base::reference;
+    static constexpr int dim() noexcept { return Dim; }
+};
+
+// 3D array: outer size_1, middle size_2, inner size_3
+template <typename Type, std::size_t size_1, std::size_t size_2, std::size_t size_3>
+class A3D : public A1D<A2D<Type, size_2, size_3>, size_1, 3> {
+public:
+    static constexpr int Dim = 3;
+    static constexpr std::size_t Size1 = size_1;
+    static constexpr std::size_t Size2 = size_2;
+    static constexpr std::size_t Size3 = size_3;
+    using base = A1D<A2D<Type, size_2, size_3>, size_1, 3>;
+    static constexpr int dim() noexcept { return Dim; }
+};
+
+// 4D array: outer size_1, then size_2, size_3, size_4
+template <typename Type, std::size_t size_1, std::size_t size_2, std::size_t size_3, std::size_t size_4>
+class A4D : public A1D<A3D<Type, size_2, size_3, size_4>, size_1, 4> {
+public:
+    static constexpr int Dim = 4;
+    using base = A1D<A3D<Type, size_2, size_3, size_4>, size_1, 4>;
+    static constexpr int dim() noexcept { return Dim; }
+};
